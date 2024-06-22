67a59735a6b5f783cc58d119ecaca1e431992e86
Masudur <masudurrahman01101993@gmail.com>
2024-06-22 22:32:11 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(){
+        return respons()->$data([
+            
+        ]);
+    }
+    // public function index($id){
+
+    // //   $name = "Donal Trump";
+    // //   $age = "75";
+    // //   return  view('index',['Id = '.$id,'name' => $name, 'age' => $age]);
+
+    // }
+
+
+}
